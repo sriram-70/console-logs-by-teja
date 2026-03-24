91a7d722b7ca6ad9d2a5ecda8580f629de5a31ca
TEJA SRI RAM <teja@TEJAs-MacBook-Pro.local>
2026-03-25 00:37:26 +0530

    feat(analytics): add microsoft clarity tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -199,6 +199,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable} ${signatureFont.variable}`}>
       <head>
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w0vlhbf1lr");
+            `
+          }}
+        />
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
